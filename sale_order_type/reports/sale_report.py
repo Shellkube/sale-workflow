73a68e513c6666e119eab6872936f9b5647c3d2a
KubeAl <96569040+KubeAl@users.noreply.github.com>
2023-11-26 21:18:13 +0530

    Added partner invoice id to sale report
    This is required for secondary sale to work

--- sale_order_type/reports/sale_report.py
+++ sale_order_type/reports/sale_report.py
@@ -10,15 +10,20 @@ class SaleReport(models.Model):
         comodel_name="sale.order.type",
         string="Type",
     )
-
+    partner_invoice_id = fields.Many2one(
+        comodel_name="res.partner",
+        string="Invoice Address",
+    )
     # flake8: noqa
     # pylint:disable=dangerous-default-value
     def _select_additional_fields(self):
         res = super()._select_additional_fields()
         res["type_id"] = "s.type_id"
+        res["partner_invoice_id"] = "s.partner_invoice_id"
         return res
 
     def _group_by_sale(self):
         res = super()._group_by_sale()
         res += """, s.type_id"""
+        res += """, s.partner_invoice_id"""
         return res
